cbdb9ca939492755b2231bc7d309d60911e98ef1
RocioGuiral <127322629+RocioGuiral@users.noreply.github.com>
2023-03-26 22:08:29 -0300

    add url back

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -9,7 +9,7 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://backend-uk5g.onrender.com/personas/';
 
   constructor(private httpClient: HttpClient) { }
 
